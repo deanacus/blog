ec802fb40d8d5c3f8d010acc8e802045acf4f0db
Dean Harris <dean@harris.tc>
2018-08-04 13:04:01 +1000

    Initial commit

---
+++ about/gear/index.md
@@ -0,0 +1,36 @@
+---
+layout: page
+title: Gear
+---
+
+## Hardware
+
+* Mid 2014 MacBook Pro with Retina Display
+* iPhone 7+
+* Plantronics BackBeat Pro 2
+
+## Software
+
+* MacOS
+* Visual Sudio Code
+* Hyper
+* Lacona
+* 1Password
+* Spectacle
+* Google Chrome
+
+## Development Stack
+
+* Node
+* Yarn
+* Webpack
+* Plain JS
+* HTML
+* SCSS
+* Occasionally some React
+
+## Skateboarding
+
+* Deck: Blind
+* Trucks: Tensor
+* Wheels: Blind
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require( 'path' );
+
+const config = {
+    entry: [
+        path.resolve( __dirname, './_src/js/app.js' ),
+        path.resolve( __dirname, './_src/scss/style.scss' ),
+    ],
+    output: {
+        path: path.resolve( __dirname, 'assets' ),
+        filename: 'app.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        minified: true,
+                        presets: [
+                            'env',
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].css',
+                        },
+                    },
+                    {
+                        loader: 'extract-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx',
+        ],
+    },
+};
+
+module.exports = config;
